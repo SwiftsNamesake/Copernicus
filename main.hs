c4451bafb8fa96d74bcdc8ac6f23e57090295382
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-11-25 13:59:01 +0100

    1) Initial commit (.gitignore, main.hs)

---
+++ main.hs
@@ -0,0 +1,19 @@
+--
+-- Main.hs - Copernicus
+-- Entry point for the Copernicus project
+--
+-- Jonatan H Sundqvist
+-- November 25 2014
+--
+
+-- TODO | - Model planetary motion and physics in 2D
+--        -
+
+-- SPEC | -
+--        -
+
+module Main where
+
+
+main = do
+	putStrLn "Hello World"
