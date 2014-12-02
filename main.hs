169c2b34c951f1ea251ec57539acfaba6fd9fc0a
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-12-02 20:01:34 +0100

    1) Added TODOs 2) Refactored a couple of functions 3) Added more bodies

--- main.hs
+++ main.hs
@@ -7,7 +7,9 @@
 --
 
 -- TODO | - Model planetary motion and physics in 2D
---        -
+--        - Cartoon earth with gravitational field
+--        - Flexible event handling
+--        - App typeclass (run, manage events, window properties, etc.)
 
 -- SPEC | -
 --        -
@@ -22,7 +24,7 @@ module Main where
 -- We'll need these
 ---------------------------------------------------------------------------------------------------
 import Graphics.Gloss.Data.Picture (line)
-import Graphics.Gloss (circleSolid)
+import Graphics.Gloss (circleSolid, rectangleSolid)
 import Graphics.Gloss.Geometry.Angle (degToRad, radToDeg, normaliseAngle)
 import Graphics.Gloss.Interface.IO.Game
 import Graphics.Gloss.Data.Vector
@@ -39,7 +41,11 @@ clampAngle = radToDeg . normaliseAngle . degToRad
 
 --
 parabola :: Float -> Vector -> Vector -> Vector -> Vector
-parabola t v a p = let (px,py) = p; (vx,vy) = v; (ax,ay) = a in (px + vx*t + 0.5*ax*t**2, py + vy*t + 0.5*ay*t**2)
+parabola t p v a = (px + vx*t + 0.5*ax*t**2, py + vy*t + 0.5*ay*t**2)
+	where
+		(px,py) = p
+		(vx,vy) = v
+		(ax,ay) = a
 
 
 -- Make typeclass (mesh, body, bounding box, collision, etc) (?)
@@ -48,7 +54,24 @@ data Body = Body Vector Vector Vector
 
 --
 animate :: Float -> Body -> Body
-animate t (Body p v a) = Body (parabola t v a p) (v + mulSV t v) a
+animate t (Body p v a) = collide (30+30/2-540/2) $ Body (parabola t p v a) (v + mulSV t a) a
+
+
+-- collide
+-- Very primitive for now
+collide :: Float -> Body -> Body
+collide gnd (Body (px, py) (vx, vy) a) = Body (px, py) (invertIf (\ v -> (px <= 15-720/2) || ( px >= (720/2-30/2))) vx, invertIf (\ v -> (v < 0) && (py <= gnd)) vy) a
+	where invertIf p v
+		| p v 		= -v
+		| otherwise =  v
+
+
+
+-- Transforms a vector from one coordinate space to another
+-- by applying the given scaling and translation
+-- Useful for converting between simulation and screen coordinates
+transform :: Vector -> Vector -> Picture -> Picture
+transform sc tr = uncurry scale sc . uncurry translate tr
 
 
 
@@ -65,15 +88,26 @@ simulate = playIO
 	respond 	-- User interaction
 	advance 	-- Advances the world to the next simulation step
 	where
-		display  			  = InWindow "Simulator" (740, 540) (25, 25)
-		world 	 			  = (0 :+ 0, v)
-		render (re:+im, _) 	  = return $ pictures [color red . translate (re) (im) $ circleSolid 15]
+		display  			  = InWindow "Simulator" (width, height) (25, 25)
+		world 	 			  = map (\ (p, v, g) -> Body p v g) [((0.0,0.0), v, g), ((10.0,0.0), (-20.0, 15.0), g), ((35.0,-28.0), v, g)]
+		render w 	 		  = return . pictures $ map drawBall w ++ [drawGround w]
+		drawBall (Body (x, y) _ _)  = color red . translate x y $ circleSolid 15
+		drawGround _ 		  = translate 0 (30/2-fromIntegral height/2) . color green $ rectangleSolid (fromIntegral width) 30
 		respond e w 		  = return w
-		advance t w = return $ (parabola t (snd w) g (fst w), snd w + (t:+0.0)*g)
-		v 					  = 40.0 :+ 20.0
-		g 					  = 0.0 :+ (-9.82)
-		parabola t v a p = let (px:+py) = p; (vx:+vy) = v; (ax:+ay) = a in (px + vx*t + 0.5*ax*t**2):+(py + vy*t + 0.5*ay*t**2)
+		advance t w 		  = return . map (animate t) $ w
+		v 					  = (40.0, 20.0) --40.0 :+ 20.0
+		g 					  = (0.0, -98.2) --0.0 :+ (-9.82)
+		(width, height) 	  = (740, 540)
+		--parabola t v a p = let (px:+py) = p; (vx:+vy) = v; (ax:+ay) = a in (px + vx*t + 0.5*ax*t**2):+(py + vy*t + 0.5*ay*t**2)
 
+
+
+--
+--renderForces
+
+
+--
+--renderGrid
 
 
 
